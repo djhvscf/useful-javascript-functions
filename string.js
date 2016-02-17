d0b9323d3fdf204157363bb639a810f0c5121145
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-02-17 09:03:44 -0600

    Added useful string functions

---
+++ string.js
@@ -0,0 +1,22 @@
+/**
+* Replace the "%s" string with the respective parameter
+*
+* @param {String} str - The string that the function will replace with the parameters passed.
+* @returns {String} If the parameters are not passed to the function, it will returns an empty string; otherwise, the function returns the string replaced.
+*/
+var sprintf = function (str) {
+        var args = arguments,
+            flag = true,
+            i = 1;
+
+        str = str.replace(/%s/g, function () {
+            var arg = args[i++];
+
+            if (typeof arg === 'undefined') {
+                flag = false;
+                return '';
+            }
+            return arg;
+        });
+        return flag ? str : '';
+    };
