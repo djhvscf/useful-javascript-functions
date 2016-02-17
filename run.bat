34e97b6b5b308d61ade096bcb523d6932d0fccf6
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-02-17 11:24:13 -0600

    More generic bat

--- run.bat
+++ run.bat
@@ -1,1 +1,3 @@
-for /f %%f in ('dir /b c:\Users\dhernandez\Documents\GitHub\useful-javascript-functions\*.js') do C:\Users\dhernandez\Documents\GitHub\useful-javascript-functions\node_modules\.bin\jsdoc C:\Users\dhernandez\Documents\GitHub\useful-javascript-functions\%%f -d C:\Users\dhernandez\Documents\GitHub\useful-javascript-functions\documentation\%%f
+for /f %%f in ('dir /b %USERPROFILE%\documents\github\useful-javascript-functions\*.js') do %USERPROFILE%\documents\github\useful-javascript-functions\node_modules\.bin\jsdoc C:\Users\dhernandez\Documents\GitHub\useful-javascript-functions\%%f -d %USERPROFILE%\documents\github\useful-javascript-functions\documentation\%%f
+
+ 
